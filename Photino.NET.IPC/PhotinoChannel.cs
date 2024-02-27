e8b0221d1c86b294eb42e6e244ed9361e6db9d51
DKOJA <dkoja@ceia-spa.com>
2024-02-27 14:10:18 +0100

    Add IPC to dependency injection, Add channel subscribers

--- Photino.NET.IPC/PhotinoChannel.cs
+++ Photino.NET.IPC/PhotinoChannel.cs
@@ -1,3 +1,5 @@
+using Photino.NET.IPC.Extensions;
+
 namespace Photino.NET.IPC;
 
 public class PhotinoChannel(PhotinoWindow owner, string channelKey)
@@ -5,8 +7,32 @@ public class PhotinoChannel(PhotinoWindow owner, string channelKey)
     private readonly string _key = channelKey;
     private readonly PhotinoWindow _owner = owner;
 
+    /*  
+        TODO: Make it typesafe, problem relies on the ConcurrentDictionary, 
+        because we don't know what is the type of the payload at compile time 
+    */
+    private readonly List<Action<object>> _subscribers = [];
+
     public string Name => _key;
     public PhotinoWindow Window => _owner;
 
-    public void Emit<T>(T message) where T : class => _owner.SendMessage(_key, new PhotinoPayload<T>(_key, message));
+    public void Subscribe(Action<object> handler)
+    {
+        _subscribers.Add(handler);
+    }
+
+    public void Unsubscribe(Action<object> handler)
+    {
+        _subscribers.Remove(handler);
+    }
+
+    public void Emit<T>(T message) where T : class
+    {
+        _owner.SendMessage(_key, new PhotinoPayload<T>(_key, message));
+
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.Invoke(message);
+        }
+    }
 }
--- Photino.NET.IPC/PhotinoIpc.cs
+++ Photino.NET.IPC/PhotinoIpc.cs
@@ -1,46 +1,20 @@
+using System.Collections.Concurrent;
+
 namespace Photino.NET.IPC;
 
-public static class PhotinoIpcRenderer
+public class PhotinoIPCService
 {
-    private static readonly Dictionary<string, Type> _channels = [];
+    public static readonly PhotinoIPCService Instance = new();
 
-    public delegate void IpcEventHandler<TEventArgs>(PhotinoChannel sender, TEventArgs? args);
+    public ConcurrentDictionary<string, PhotinoChannel> Channels { get; } = new();
 
-    public static PhotinoWindow RegisterChannelHandler<T>(this PhotinoWindow window, string key, IpcEventHandler<T> handler) where T : class
+    public delegate void IPCEventHandler<TEventArgs>(PhotinoChannel sender, TEventArgs? args);
+
+    public void ValidateChannelKey(string key)
     {
-        _channels.TryAdd(key, typeof(T));
-
-        return window.RegisterWebMessageReceivedHandler((s, e) =>
+        if (!Channels.ContainsKey(key))
         {
-            if (s is not PhotinoWindow window) return;
-
-            if (!PhotinoPayload<T>.TryFromJson(e, out var payload)) return;
-
-            if (payload?.Key != key) return;
-
-            var channel = new PhotinoChannel(window, key);
-
-            handler.Invoke(channel, payload.Data);
-        });
-    }
-
-    public static void SendMessage<T>(this PhotinoWindow window, T message) where T : class
-    {
-        var channels = _channels
-            .Where(x => x.Value == typeof(T))
-            .Select(x => x.Key);
-
-        Parallel.ForEach(channels, channel =>
-        {
-            window.SendWebMessage(PhotinoPayload<T>.ToJson(message));
-        });
-    }
-
-    public static void SendMessage<T>(this PhotinoWindow window, string key, T message) where T : class
-    {
-        if (!_channels.ContainsKey(key))
-            return;
-
-        window.SendWebMessage(PhotinoPayload<T>.ToJson(message));
+            throw new InvalidOperationException($"Channel with key {key} does not exist");
+        }
     }
 }
